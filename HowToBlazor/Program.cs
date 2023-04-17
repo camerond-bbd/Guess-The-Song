173ab81998bcdc45b69c9d99e983d243be2767c6
Manuel <74509303+Manlx@users.noreply.github.com>
2023-04-18 00:43:02 +0200

    Added project to seperate components

--- HowToBlazor/Program.cs
+++ HowToBlazor/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+var logger = builder.Logging;
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
---
+++ Razor.Utils.Components/ActorListComp.razor.cs
@@ -0,0 +1,28 @@
+﻿using HowToBlazor.Utils;
+using Microsoft.Extensions.Logging;
+
+namespace Razor.Utils.Components
+{
+    public partial class ActorListComp
+    {
+        public List<string> lstActors = new List<string>();
+
+        public string inValue = "Invalue is ";
+        public void addActor(string Value)
+        {
+            //this.lstActors.Add(Value);
+            inValue = Value;
+            genPage();
+        }
+
+        public string genPage()
+        {
+            return this.inValue;
+        }
+
+        public ActorListComp()
+        {
+            PubSubManager.Subscribe<string>("addedActor", this.addActor);
+        }
+    }
+}
