3e03a7bb9e794f804a22c2dba62fb48676af34ef
Manuel Nunes <manuel.nunes@vcontractor.co.za>
2023-04-17 16:05:42 +0200

    Old MVC Files

---
+++ MVC Exmaple/Controllers/IndexController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Exmaple.Models;
+
+namespace MVC_Exmaple.Controllers;
+
+public class IndexController : Controller
+{
+    public IndexController()
+    {
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ MVC Exmaple/Models/UserModel.cs
@@ -0,0 +1,8 @@
+namespace MVC_Exmaple.Models;
+
+public class UserModel
+{
+    public string? UserName { get; set; }
+    public int Age { get; set; }
+    public int ID { get; set; }
+}
