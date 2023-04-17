10f82f0b18a19bd603430269b7f015eb506f6b1d
Manuel Nunes <manuel.nunes@vcontractor.co.za>
2023-04-17 19:04:26 +0200

    Started Testing PubSub with Blazor

---
+++ HowToBlazor/Components/ActorAddComp.cs
@@ -0,0 +1,21 @@
+﻿using HowToBlazor.Utils;
+using Microsoft.AspNetCore.Components;
+
+namespace HowToBlazor.Components
+{
+    public partial class ActorAddComp: ComponentBase
+    {
+        public List<string> lstActors = new List<string>();
+        public PubSubEvent<string> addActorDel;
+
+        public void addActor(string Value)
+        {
+            this.lstActors.Add(Value);
+        }
+
+        public ActorAddComp()
+        {
+            addActorDel = new PubSubEvent<string>(this.addActor);
+        }
+    }
+}
---
+++ HowToBlazor/Utils/PubSubManager.cs
@@ -0,0 +1,16 @@
+﻿namespace HowToBlazor.Utils
+{
+    public static class PubSubManager
+    {
+        public static List<Object> lstPubSubs;
+        static PubSubManager() { 
+            lstPubSubs = new List<Object>();
+            lstPubSubs.Add(new PubSub<string>());
+        }
+
+        void Publish<T>()
+        {
+
+        }
+    }
+}
