4cec9dc8f1e8e64e22f517978248e865a48de61a
Manuel Nunes <manuel.nunes@vcontractor.co.za>
2023-04-18 23:07:45 +0200

    Added some files

--- Razor.Utils.Components/ActorListComp.razor.cs
+++ Razor.Utils.Components/ActorListComp.razor.cs
@@ -8,11 +8,14 @@ namespace Razor.Utils.Components
         public List<string> lstActors = new List<string>();
 
         public string inValue = "Invalue is ";
+        public string displayData = "";
+        public string temp = "my Temp";
         public void addActor(string Value)
         {
             //this.lstActors.Add(Value);
             inValue = Value;
             genPage();
+            this.StateHasChanged();
         }
 
         public string genPage()
@@ -23,6 +26,11 @@ namespace Razor.Utils.Components
         public ActorListComp()
         {
             PubSubManager.Subscribe<string>("addedActor", this.addActor);
+        }
+
+        public void clickUpdate()
+        {
+            displayData = $"Current Data is: {this.inValue}" ;
         }
     }
 }
