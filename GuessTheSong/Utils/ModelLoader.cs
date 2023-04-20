9b33fa89f1f4b449a804e9326c11463c8b27d0a8
Michael Coker <michaelco@bbd.co.za>
2023-04-20 15:30:38 +0200

    added scripts to insert into database

--- GuessTheSong/Utils/ModelLoader.cs
+++ GuessTheSong/Utils/ModelLoader.cs
@@ -65,5 +65,22 @@ namespace GuessTheSong.Utils
             Songs song = new Songs(result.songTitle, result.artistId, result.genreId);
             return song;
         }
+
+         public void addScore(bool wonRound,int player_id){
+            if(wonRound){
+                query = "INSERT INTO scores(player_score) VALUES(1)";
+            }else{
+                query = "INSERT INTO scores(player_score) VALUES(0)";
+            }
+            DatabaseHandler.execute(query);
+         }
+
+         public void AddPlayer(string username, string password){
+            string query = "INSERT INTO players (username,player_password) VALUES (\'"+username+"\',\'"+password"+\')";
+            DatabaseHandler.execute(query);
+         }
+
+
+
     }
 }
