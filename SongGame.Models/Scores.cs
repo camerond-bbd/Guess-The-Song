af7cb09ef1cf4b8e884193ff6e8572b0498aef8d
Cameron <122345679+camerond-bbd@users.noreply.github.com>
2023-04-20 14:19:58 +0200

    Constructors for Models created

--- SongGame.Models/Scores.cs
+++ SongGame.Models/Scores.cs
@@ -4,8 +4,12 @@
     {
         public int score_id { get; set; }
         public int player_id { get; set; }
-        public int song_id { get; set; }
-        public int lyric_id { get; set; }
         public int player_score { get; set; }
+
+        public Scores (int playerId, int playerScore)
+        {
+            player_id = playerId;
+            player_score = playerScore;
+        }
     }
 }
