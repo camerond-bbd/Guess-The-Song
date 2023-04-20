3a048c69d8886b3e152a2e5f685dc704ba031246
Cameron <122345679+camerond-bbd@users.noreply.github.com>
2023-04-20 15:27:08 +0200

    syntax fix

--- GuessTheSong/Utils/ModelLoader.cs
+++ GuessTheSong/Utils/ModelLoader.cs
@@ -6,61 +6,53 @@ namespace GuessTheSong.Utils
 {
     public class ModelLoader
     {
-       
-       private Artists artist;
-       private Genres genre;
-       private Lyrics lyrics;
-       private Players player;
-       private Scores score;
-       private Songs song;
-
-       public Artists getArtist(String id){
+       public Artists getArtist(string id){
         Dictionary<string, string> result = new Dictionary<string, string>();
-        String query = "SELECT [name] FROM artists WHERE artist_id = " + id;
+        string query = "SELECT [name] FROM artists WHERE artist_id = " + id;
         result = DatabaseHandler.execute(query);
-        Artists artist = new Artist(result.name);
+        Artists artist = new Artists(result.name);
         return artist;
        }
 
-       public Genres getGenre(String id){
+       public Genres getGenre(string id){
             Dictionary<string, string> result = new Dictionary<string, string>();
-            String query = "SELECT name FROM artists WHERE artist_id = " + id;
+            string query = "SELECT name FROM genres WHERE genre_id = " + id;
             result = DatabaseHandler.execute(query);
             Genres genres = new Genres(result.name);
             return genres;
        }
 
-        public Lyrics getLyrics(String id)
+        public Lyrics getLyrics(string id)
         {
             Dictionary<string, string> result = new Dictionary<string, string>();
-            String query = "SELECT lyric_text FROM lyrics WHERE lyric_id = " + id;
+            string query = "SELECT lyric_text FROM lyrics WHERE lyric_id = " + id;
             result = DatabaseHandler.execute(query);
             Lyrics lyrics = new Lyrics(result.name);
             return lyrics;
         }
 
-        public Players getPlayers(String id)
+        public Players getPlayers(string id)
         {
             Dictionary<string, string> result = new Dictionary<string, string>();
-            String query = "SELECT username FROM players WHERE player_id = " + id;
+            string query = "SELECT username FROM players WHERE player_id = " + id;
             result = DatabaseHandler.execute(query);
             Players players = new Players(result.username, result.playerPassword);
             return players;
         }
 
-        public Scores getScore(String id)
+        public Scores getScore(string id)
         {
             Dictionary<string, string> result = new Dictionary<string, string>();
-            String query = "SELECT player_score FROM scores WHERE player_id = " + id;
+            string query = "SELECT player_score FROM scores WHERE player_id = " + id;
             result = DatabaseHandler.execute(query);
             Scores scores = new Scores(result.playerScore);
             return scores;
         }
 
-        public Songs getSongs(String id)
+        public Songs getSongs(string id)
         {
             Dictionary<string, string> result = new Dictionary<string, string>();
-            String query = "SELECT title, artist_id, genre_id FROM songs WHERE song_id = " + id;
+            string query = "SELECT title, artist_id, genre_id FROM songs WHERE song_id = " + id;
             result = DatabaseHandler.execute(query);
             Songs song = new Songs(result.songTitle, result.artistId, result.genreId);
             return song;
