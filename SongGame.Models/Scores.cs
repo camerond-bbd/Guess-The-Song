7b83d68adca42ee357b2306f63dc023c0108aab2
Cameron <122345679+camerond-bbd@users.noreply.github.com>
2023-04-20 12:10:36 +0200

    Scores Model added

--- SongGame.Models/Scores.cs
+++ SongGame.Models/Scores.cs
@@ -2,6 +2,10 @@
 {
     public class Scores
     {
-
+        public int score_id { get; set; }
+        public int player_id { get; set; }
+        public int song_id { get; set; }
+        public int lyric_id { get; set; }
+        public int player_score { get; set }
     }
 }
