d19d77e78bc3428aedb3f713a368c8d1cf782f4d
Manuel Nunes <manuel.nunes@vcontractor.co.za>
2023-04-20 18:19:02 +0200

    added popup message

---
+++ GuessTheSong/components/PopupMessage.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GuessTheSong.components
+{
+    public partial class PopupMessage
+    {
+        public string errorMessage = "";
+        
+        [Parameter]
+        public bool isDisplayingMessage { get; set; }
+        public string getClasses()
+        {
+            return (isDisplayingMessage) ? $"popupMessage" : $"popupMessage noDisplay";
+        }
+    }
+}
