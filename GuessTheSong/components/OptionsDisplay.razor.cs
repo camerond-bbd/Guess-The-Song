47ff53dc1e00c52187ca300acc1071a394463a46
Manuel Nunes <manuel.nunes@vcontractor.co.za>
2023-04-21 12:19:32 +0200

    Added Game Manager

---
+++ GuessTheSong/components/OptionsDisplay.razor.cs
@@ -0,0 +1,33 @@
+﻿using GuessTheSong.Utils;
+using SongGame.Models;
+
+namespace GuessTheSong.components
+{
+    public partial class OptionsDisplay
+    {
+
+        public class OptionsDisplayDataPass
+        {
+            public Artists artist { get; set; } = new Artists();
+            public Songs song { get; set; } = new Songs();
+            public Genres genre { get; set; } = new Genres();
+        }
+        public Artists artist { get; set; }= new Artists();
+        public Songs song { get; set; } = new Songs();
+        public Genres genre { get; set; } = new Genres();
+        
+      
+        public void onClick()
+        {
+            OptionsDisplayDataPass passData = new OptionsDisplayDataPass();
+            passData.song = song;
+            passData.artist = artist; 
+            passData.genre = genre;
+            PubSubManager.Publish<OptionsDisplayDataPass>("optionWasClicked", passData);
+        }
+
+        OptionsDisplay() { 
+            //PubSubManager.Subscribe<OptionsDisplayDataPass>()
+        }
+    }
+}
