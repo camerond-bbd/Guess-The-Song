7c0b45a0e2d92b785c76aa08efa990a7a9ca7395
Manuel Nunes <manuel.nunes@vcontractor.co.za>
2023-04-21 09:03:49 +0200

    Added gamePage

--- GuessTheSong/Pages/Profile.razor.cs
+++ GuessTheSong/Pages/Profile.razor.cs
@@ -1,19 +1,29 @@
 ﻿using GuessTheSong.components;
 using GuessTheSong.Utils;
 using Microsoft.AspNetCore.Components;
+using SongGame.Models;
 
 namespace GuessTheSong.Pages
 {
     public partial class Profile
     {
         [Parameter]
-        public int? Id { get; set; }
+        public int Id { get; set; }
+        public Players loggedinUser { get; set; }
         public Profile()
         {
+            loggedinUser = new Players();
             string errorMessage = "User not found";
             bool displayError = true;
-
+            if (ID == 0)
+                return;
+            loggedinUser = ModelLoader.getPlayers(Id);
             //PubSubManager.Publish<PopupMessage.errorObject>("displayErrorMessage", new PopupMessage.errorObject(errorMessage, displayError, () => { }));
+        }
+
+        public void goToGameClick()
+        {
+
         }
     }
 }
